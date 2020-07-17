2bba63d7363fd77296e9fcd33dc1722bfd3af93d
Dannus90 <persson.daniel.1990@gmail.com>
2020-07-17 20:56:05 +0200

    Update PostCommand Item & Get single item by Id

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CmdApi.Models;
 
 namespace CmdApi.Controllers
 {
@@ -7,10 +8,41 @@ namespace CmdApi.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
+        private readonly CommandContext _context;
+
+        public CommandsController(CommandContext context) => _context = context;
+
+
+        //Get: api/commands
         [HttpGet]
-        public ActionResult<IEnumerable<string>> GetString()
+        public ActionResult<IEnumerable<Command>> GetCommands()
         {
-            return new string[] { "this", "is", "hard", "coded" };
+            return _context.CommandItems;
+        }
+
+        //Get: api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandItem(int id)
+        {
+            var commandItem = _context.CommandItems.Find(id);
+
+            if (commandItem == null)
+            {
+                return NotFound();
+            }
+
+            return commandItem;
+        }
+
+
+        //POST: api/commands
+        [HttpPost]
+        public ActionResult<Command> PostCommandItem(Command command)
+        {
+            _context.CommandItems.Add(command);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetCommandItem", new Command { Id = command.Id }, command);
         }
     }
 }
